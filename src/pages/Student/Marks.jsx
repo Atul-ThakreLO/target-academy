df6820f8d2bb56c858ca5bc2cc8b0ee668e405a5
Atul-ThakreLO <atulthakrelrat3094@gmail.com>
2025-05-18 19:28:16 +0530

    trying to vercel issue

--- src/pages/Student/Marks.jsx
+++ src/pages/Student/Marks.jsx
@@ -3,15 +3,14 @@ import MarksTable from "@/components/Student/Marks/Marks-table";
 import React from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useGetMarksForStudent } from "@/Hooks/use-marks";
-import { Loader2 } from "lucide-react";
 
 const Marks = () => {
-  const { data, isLoading, isFetched } = useGetMarksForStudent();
+  const { data, isLoading } = useGetMarksForStudent();
   return (
     <ScrollArea className="h-[95vh]">
       <>
         <MarksTable data={data?.data} isLoading={isLoading} />
-        <h5 className="text-4xl mt-20  font-medium">
+        <h5 className="text-4xl mt-20 p-3 md:p-6 font-medium">
           Graphical Representation
         </h5>
         <Chart />
